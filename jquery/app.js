0f99fdb8db82d34d996b4c64fd5ae9225f0f5c5e
kajalmia349 <kajalmia3490@gamil.com>
2023-06-27 02:07:27 +0600

    'book'

---
+++ jquery/app.js
@@ -0,0 +1,73 @@
+// page reload
+function pageReload() {
+    location.reload(true);
+};
+
+// add to cart & total cart
+let carts = [];
+
+function added(item) {
+    $('#added-cart').html(carts.push(item));
+};
+
+
+function buttonClick1() {
+    $('.btn1').on('click', $('.btn1').html('Added'));
+    added();
+};
+
+
+function buttonClick2() {
+    $('.btn2').on('click', $('.btn2').html('Added'));
+    added();
+};
+
+function buttonClick3() {
+    $('.btn3').on('click', $('.btn3').html('Added'));
+    added();
+};
+
+function buttonClick4() {
+    $('.btn4').on('click', $('.btn4').html('Added'));
+    added();
+};
+
+function buttonClick5() {
+    $('.btn5').on('click', $('.btn5').html('Added'));
+    added();
+};
+
+function buttonClick6() {
+    $('.btn6').on('click', $('.btn6').html('Added'));
+    added();
+};
+
+function buttonClick7() {
+    $('.btn7').on('click', $('.btn7').html('Added'));
+    added();
+};
+
+function buttonClick8() {
+    $('.btn8').on('click', $('.btn8').html('Added'));
+    added();
+};
+
+function buttonClick9() {
+    $('.btn9').on('click', $('.btn9').html('Added'));
+    added();
+};
+
+function buttonClick10() {
+    $('.btn10').on('click', $('.btn10').html('Added'));
+    added();
+};
+
+function buttonClick11() {
+    $('.btn11').on('click', $('.btn11').html('Added'));
+    added();
+};
+
+function buttonClick12() {
+    $('.btn12').on('click', $('.btn12').html('Added'));
+    added();
+};
